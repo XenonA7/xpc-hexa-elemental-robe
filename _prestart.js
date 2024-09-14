4574bdcd69bf1b1f8b31f17a047d52b2e677fb86
XenonA7 <105614278+XenonA7@users.noreply.github.com>
2024-09-14 15:02:34 -0700

    1.0.0
    Initial Release

---
+++ _prestart.js
@@ -0,0 +1,8 @@
+{
+	elementalhair.AddAsset("media/entity/npc/hexacast1.png", "hexacast-palette.png");
+	elementalhair.AddAsset("media/entity/npc/hexacast1-sweeps.png", "hexacast-palette.png");
+	elementalhair.AddAsset("media/entity/npc/hexacast1-triblader.png", "hexacast-palette.png");
+	elementalhair.AddAsset("media/entity/npc/hexacast2.png", "hexacast-palette.png");
+	elementalhair.AddAsset("media/entity/npc/hexacast2-sweeps.png", "hexacast-palette.png");
+	elementalhair.AddAsset("media/entity/npc/hexacast2-triblader.png", "hexacast-palette.png");
+}
